eb8143e1c3a166a9e0b2169e800530b4d5d2e35b
Tobi Taseyon Denagan <ttdenagan@sidmach.com>
2021-12-31 16:37:19 +0100

    Custom Authentication - DB Authentication

---
+++ src/main/java/com/example/springsecurity/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,63 @@
+package com.example.springsecurity.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.example.springsecurity.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUser()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    public List<ApplicationUser> getApplicationUser(){
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        "tobi",
+                        passwordEncoder.encode("password"),
+                        STUDENT.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "ade",
+                        passwordEncoder.encode("password"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "laide",
+                        passwordEncoder.encode("password"),
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+        return applicationUsers;
+    }
+}
--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,17 +1,16 @@
 package com.example.springsecurity.security;
 
+import com.example.springsecurity.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
@@ -24,10 +23,13 @@ import static com.example.springsecurity.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -77,34 +79,47 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails tobiUser = User.builder()
-                .username("tobi")
-                .password(passwordEncoder.encode("password1"))
-//                .roles(STUDENT.name()) //ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails adeUser = User.builder()
-                .username("ade")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name()) //ROLE_ADMIN
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails laideUser = User.builder()
-                .username("laide")
-                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                tobiUser,
-                adeUser,
-                laideUser
-        );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails tobiUser = User.builder()
+//                .username("tobi")
+//                .password(passwordEncoder.encode("password1"))
+////                .roles(STUDENT.name()) //ROLE_STUDENT
+//                .authorities(STUDENT.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails adeUser = User.builder()
+//                .username("ade")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMIN.name()) //ROLE_ADMIN
+//                .authorities(ADMIN.getGrantedAuthorities())
+//                .build();
+//
+//        UserDetails laideUser = User.builder()
+//                .username("laide")
+//                .password(passwordEncoder.encode("password123"))
+////                .roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(
+//                tobiUser,
+//                adeUser,
+//                laideUser
+//        );
+//    }
 
 }
